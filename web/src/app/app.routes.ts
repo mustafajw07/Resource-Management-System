d3be24fe226134ce499c66138ddbfd80b242b3d3
Mustafa <mustafa.jawadwala@godigitaltc.com>
2025-12-08 15:41:33 +0530

    remove unwanted code

--- web/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LayoutComponent } from './pages/layout/layout.component';
-
-export const routes: Routes = [
-    {
-        path:'',
-        component: LayoutComponent
-    }
-];
