c0e823489f3408d300214dade0fb263ada1eade7
Mustafa <mustafa.jawadwala@godigitaltc.com>
2026-01-12 16:07:17 +0530

    optimization

--- rms-web/src/app/pages/project-utilization/project-utilization.component.ts
+++ rms-web/src/app/pages/project-utilization/project-utilization.component.ts
@@ -12,14 +12,15 @@ import { UtilizationData } from '@core/interfaces/project';
   imports: [ProjectUtilizationTableComponent, SelectButtonModule, FormsModule],
 })
 export class ProjectUtilizationComponent implements OnInit{
-  value!: number;
-  filterOptions = [
+  protected value!: number;
+  protected filterOptions = [
     { icon: 'pi pi-user', name: 'User', value: 1 },
     { icon: 'pi pi-folder', name: 'Projects', value: 2 }
   ];
   protected projectUtilizationData: UtilizationData[] = [];
   protected filterByProject = false;
   protected filterByUser = false;
+
   private readonly projectService = inject(ProjectService);
   
   ngOnInit(): void {
@@ -33,17 +34,13 @@ export class ProjectUtilizationComponent implements OnInit{
     });
   }
 
-
-  protected handleSelectionChange(){
-    if(this.value === 1){
-      this.filterByUser = true;
-      this.filterByProject = false;
-    } else if(this.value === 2){
-      this.filterByUser = false;
-      this.filterByProject = true;
-    }else{
-      this.filterByUser = false;
-      this.filterByProject = false;
-    }
+  /**
+   * Handles the change in selection for filtering options.
+   * @protected
+   * @returns void
+   */
+  protected handleSelectionChange(): void {
+    this.filterByUser = this.value === 1;
+    this.filterByProject = this.value === 2;
   }
 }
